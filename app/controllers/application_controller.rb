38f66b15b0a5891c073d152d1609747b91c71c75
Ainsley M Galvez <galvezainsley@gmail.com>
2022-10-05 17:28:31 +0000

    finished scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,52 @@ class ApplicationController < ActionController::Base
   # ================================
 
   def play_rock
+
+    index = rand(3)
+     moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.at(index)
+
+    if  @comp_move == "rock"
+        @outcome = "tied"
+    elsif @comp_move == "paper"
+        @outcome = "lost"
+    elsif @comp_move == "scissors"
+        @outcome = "won"
+    end
+
     render({:template => "game_templates/rock.html.erb"})
   end
 
   def play_paper
+    index = rand(3)
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.at(index)
+    if @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    elsif @comp_move == "rock"
+      @outcome = "win"
+    end
+
     render({:template => "game_templates/paper.html.erb"})
   end
 
+  def play_scissors 
+    index = rand(3)
+    moves = ["rock", "paper", "scissors"]
+
+    @comp_move = moves.at(index)
+
+    if @comp_move == "scissors"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "rock"
+      @outcome = "lost"
+
+    end
+      
+      render({:template => "game_templates/scissors.html.erb"})
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   get("/paper", { :controller => "application", :action => "play_paper"})
 
+  get("/scissors", {:controller => "application", :action => "play_scissors"})
+
 end
